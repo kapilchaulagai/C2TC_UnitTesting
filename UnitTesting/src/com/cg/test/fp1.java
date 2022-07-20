c392dcb249748be805c0b61e57f07a362d4bf0db
cw_51 <cw_51@ASUS-BEAST>
2022-07-20 23:11:12 +0530

    Commit on 20-7-2022

---
+++ UnitTesting/src/com/cg/test/fp1.java
@@ -0,0 +1,23 @@
+package com.cg.test;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Disabled;
+
+public class fp1 {
+	@Test
+	public void disp()
+	{
+		System.out.println("Hi");
+	}
+	@Test
+	public void disp1()
+	{
+		System.out.println("Hello");
+	}
+	@Disabled
+	@Test
+	public void disp2()
+	{
+		System.out.println("Hi Everyone!");
+	}
+}
